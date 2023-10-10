1a476929e37d00be4165ba18d75e5e054f55f1b2
viswak <63440120+viswakbaddie@users.noreply.github.com>
2023-10-10 17:33:56 +0300

    Update game.py

--- flight simulator/game/game.py
+++ flight simulator/game/game.py
@@ -3,55 +3,55 @@ import random
 from geopy import distance
 con=mysql.connect(host='localhost', user='root',password='password',database='flight_game')
 cursor=con.cursor()
-current_airport=None
-startingairport=None
-print('<game description>')
-#create table credentials(name varchar(20),password varchar(20));
-start=1000
-distanc=0
-l=[100,50,0,-50,-100]
-def starting_airport():
-    global current_airport
-    a=random.randint(1,70942)
-    querry='select name from airport;'
-    cursor.execute(querry)
-    b=cursor.fetchall()
-    current_airport=b[a][0]
-    return current_airport
-    
-def airportscloseby():
-    global current_airport,start,distanc,startingairport
-    querry1='select name,latitude_deg,longitude_deg from airport;'
+start=2000
+score=0
+#Good Samaritan Hospital Heliport
+l=[0,100,200,-100,-200]
+a = random.randint(1, 70942)
+querry = 'select name from airport;'
+cursor.execute(querry)
+b = cursor.fetchall()
+current_airport = b[a][0]
+while start>0:
+
+    print('you are at:', current_airport)
+    querry1 = 'select name,latitude_deg,longitude_deg from airport;'
     cursor.execute(querry1)
-    a=cursor.fetchall()
-    startingairport = starting_airport()
-    querry2=f'select latitude_deg,longitude_deg from airport where name="{startingairport}"'
+    a = cursor.fetchall()
+    querry2 = f'select latitude_deg,longitude_deg from airport where name="{current_airport}"'
     cursor.execute(querry2)
-    data=cursor.fetchone()
+    data = cursor.fetchone()
     for i in a:
         loc1 = data
         loc2 = (i[1], i[2])
         distanc=distance.distance(loc1, loc2).miles
-        if distanc<10:
+        if distanc<15:
             print(i[0])
-    current_airport=input('copy and paste the airport you want to go to')
-    start=start-(distance1()*50)
-    x=money_add()
-    print(f'money status from new airport:{x}')
-    start=start+x
-    print(f'money remaining={int(start)}')
-    
-def distance1():
-    global current_airport
-    querry2 = f'select latitude_deg,longitude_deg from airport where name="{startingairport}"'
+    current_airport = input('copy and paste the airport you want to go to')
+    querry2 = f'select latitude_deg,longitude_deg from airport where name="{current_airport}"'
     cursor.execute(querry2)
-    loc1 = cursor.fetchone()
-    querry=f'select latitude_deg,longitude_deg from airport where name="{current_airport}"'
-    cursor.execute(querry)
-    loc2 = cursor.fetchone()
-    x = distance.distance(loc1, loc2).miles
-    return x
-    
-def money_add():
+    data1 = cursor.fetchone()
+    loc1 = data
+
+
+    loc2=data1
+    distanc = distance.distance(loc1, loc2).miles
     x=random.randint(0,4)
-    return l[x]
+    money=l[x]
+    if money>0:
+        print('money gained=',money)
+    elif money<0:
+        print('money lost=',money)
+    else:
+        print('no loss no gain')
+
+    start=start-(distanc*100)+money
+    if start<0:
+        print('ran out of money')
+    else:
+        print('current wallet',int(start))
+
+
+    score=score+distanc
+    print(f'distance traveled: {score:.2f}')
+
